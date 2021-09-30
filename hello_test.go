38a1380f8fe13acb8593c4ee1997b89091677feb
Suriya Kalivardhan <suriyak@microsoft.com>
2021-09-29 18:20:01 -0700

    Allocate request strucutre and tests

--- hello_test.go
+++ hello_test.go
@@ -8,3 +8,42 @@ func TestHello(t *testing.T) {
 		t.Fatalf("Unexepcted result %s", result)
 	}
 }
+
+func TestSampleRequest(t *testing.T) {
+	request := getSampleRquest()
+	if request.id != "suriya" && request.target != 34 {
+		t.Fatalf("Unexpected result %s, %v", request.id, request.target)
+	}
+
+	nextRequest := copyRequest(request)
+	nextRequest.id = "magi"
+	nextRequest.target = 31
+
+	if request.id != "suriya" || request.target != 34 {
+		t.Fatalf("Unexpected result %s, %v", request.id, request.target)
+	}
+
+	if nextRequest.id != "magi" || nextRequest.target != 31 {
+		t.Fatalf("Unexpected result %s, %v", nextRequest.id, nextRequest.target)
+	}
+
+	pointerRequest := request
+	if request.id != "suriya" || request.target != 34 {
+		t.Fatalf("Unexpected result %s, %v", request.id, request.target)
+	}
+
+	if pointerRequest.id != "suriya" || pointerRequest.target != 34 {
+		t.Fatalf("Unexpected result %s, %v", pointerRequest.id, pointerRequest.target)
+	}
+
+	pointerRequest.id = "agazhi"
+	pointerRequest.target = 4
+	if request.id != "agazhi" || request.target != 4 {
+		t.Fatalf("Unexpected result %s, %v", request.id, request.target)
+	}
+
+	if pointerRequest.id != "agazhi" || pointerRequest.target != 4 {
+		t.Fatalf("Unexpected result %s, %v", pointerRequest.id, pointerRequest.target)
+	}
+
+}
--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "log"
 
+type AllocateRequest struct {
+	id     string
+	target int
+}
+
 func main() {
 	result := getHello("Agazhi")
 	log.Println(result)
@@ -9,4 +14,18 @@ func main() {
 
 func getHello(name string) string {
 	return "Hello " + name
+}
+
+func getSampleRquest() *AllocateRequest {
+	return &AllocateRequest{
+		id:     "suriya",
+		target: 34,
+	}
+}
+
+func copyRequest(request *AllocateRequest) AllocateRequest {
+	return AllocateRequest{
+		id:     request.id,
+		target: request.target,
+	}
 }
